32e83c7409320977e9696705c8d262608453a15e
Trohinic <159806490+Trohinic@users.noreply.github.com>
2024-03-02 23:45:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,39 +4,40 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value == 0 || value == 1) return false;
-  for (int i = 2; i <= sqrt(value); ++i) {
-  	if (value % i == 0) {
-  		return false;
-  	}
+ if (value == 0 || value == 1) return false;
+ for (int i = 2; i*i <= value; ++i) {
+  if (value % i == 0) {
+   return false;
   }
-  return true;
+ }
+ return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int i = 1;
-	while (n > 1) {
-		i += 1;
-		if (checkPrime(i)) {
-			n -= 1;
-		}
-	}
+ uint64_t i = 1;
+ while (n > 1) {
+  i += 1;
+  if (checkPrime(i)) {
+   n -= 1;
+  }
+ }
+ return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	value += 1;
-	while (!checkPrime(value)) {
-		value += 1;
-	}
-	return value;
+ value += 1;
+ while (!checkPrime(value)) {
+  value += 1;
+ }
+ return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sum = 0;
-	for (int i = 2; i < hbound; ++i) {
-		if (checkPrime(i)) {
-			sum += i;
-		}
-	}
-	return sum;
+ uint64_t sum = 0;
+ for (int i = 2; i < hbound; ++i) {
+  if (checkPrime(i)) {
+   sum += i;
+  }
+ }
+ return sum;
 }

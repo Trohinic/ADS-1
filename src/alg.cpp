df0f6a0b0e8c5b7edf3ffd88705895bc3470e4d1
Trohinic <159806490+Trohinic@users.noreply.github.com>
2024-03-02 23:07:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,39 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value == 0 || value == 1) return false;
+  for (int i = 2; i <= sqrt(value); ++i) {
+  	if (value % i == 0) {
+  		return false;
+  	}
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	int i = 1;
+	while (n > 1) {
+		i += 1;
+		if (checkPrime(i)) {
+			n -= 1;
+		}
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	value += 1;
+	while (!checkPrime(value)) {
+		value += 1;
+	}
+	return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for (int i = 2; i < hbound; ++i) {
+		if (checkPrime(i)) {
+			sum += i;
+		}
+	}
+	return sum;
 }

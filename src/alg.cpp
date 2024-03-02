bf04b49ab814f2027582dfc5742e9741f93289bd
Trohinic <159806490+Trohinic@users.noreply.github.com>
2024-03-03 00:23:17 +0300

    Update_fin_fin alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,48 +4,41 @@
 
 
 bool checkPrime(uint64_t value) {
-   if (value == 0 || value == 1) return false;
-   for (int i = 2; i*i <= value; ++i) {
-      if (value % i == 0) {
-         return false;
-      }
-   }
-   return true;
+  if (value == 0 || value == 1) return false;
+  for (int i = 2; i*i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-   uint64_t i = 2;
-   while (n > 1) {
-      i += 1;
-      if (checkPrime(i)) {
-         n -= 1;
-      }
-   }
-   return i;
+  uint64_t i = 2;
+  while (n > 1) {
+    i += 1;
+    if (checkPrime(i)) {
+      n -= 1;
+    }
+  }
+  return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
-   value += 1;
-   while (!checkPrime(value)) {
-      value += 1;
-   }
-   return value;
+  value += 1;
+  while (!checkPrime(value)) {
+    value += 1;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-   uint64_t sum = 0;
-   for (int i = 2; i < hbound; ++i) {
-      if (checkPrime(i)) {
-         sum += i;
-      }
-   }
-   return sum;
+  uint64_t sum = 0;
+  for (int i = 2; i < hbound; ++i) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
-int main() {
-   for (int i = 1; i < 11; ++i) {
-      cout << i << ' ' << nPrime(i) << '\n';
-   }
-   
-   return 0;
-}
